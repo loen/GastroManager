29a8be4b48c1cfdd4dfdb17046ca6bc39a2cf488
fallenspecter <luke.lesniak@gmail.com>
2017-03-27 08:34:45 +0200

    Fixed formatting of message sent to slackbot

--- bin/recipeSender.js
+++ bin/recipeSender.js
@@ -5,7 +5,7 @@ var config = require ('./config');
 
 function sendRecipe(winner, restaurant, contact, email){
     var msg = recipeCreator.prepareRecipe(winner, restaurant, contact, email);
-   restUtil.postMessage('@' + winner.name, msg, false);
+    restUtil.postRawMessage('@' + winner.name, msg, false);
     restUtil.postRawMessage(config.settings.channel, msg, true);
     pdfCreator.generatePdf(restaurant,function(fileName){
         restUtil.postPdf(fileName)});
