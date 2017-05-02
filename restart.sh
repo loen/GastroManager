983b3ebd611cdf3b43e7e89d1aa3233e39813bdd
loen <andrzej.pozlutko@gmail.com>
2017-05-02 19:08:55 +0200

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-
 PID=`ps -ef | grep '[n]ode' | awk '{print $2; exit}'`
-echo $PID
-if [ -n $PID ]
+if [[ ! -z $PID ]]
  then
-  kill -9 $PID
-fi
-./run.sh
+   kill -9 $PID
+   fi
+   ./run.sh
+
