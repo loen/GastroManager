6b68c31377d5b1cc20681ac17b8cbebafbf44c9c
loen <andrzej.pozlutko@gmail.com>
2017-05-10 09:25:07 +0200

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-PID=`ps -ef | grep '[n]ode' | awk '{print $2; exit}'`
+PID=`ps -ef | grep 'sudo [n]ode index.js' | awk '{print $2; exit}'`
 if [[ ! -z $PID ]]
  then
-   kill -9 $PID
+  sudo pkill -9 -P $PID
    fi
 cd /home/pi/GastroManager
 sudo node index.js > /home/pi/GastroManager/log.log 2>&1 &
